e7f0e3dde9dd2af8567494ae67de4d4a4c8d1272
joule18 <jrjundam18@gmail.com>
2024-11-21 15:25:24 +0800

    added tv show fetching endpoints

--- backend/contollers/tv.controller.js
+++ backend/contollers/tv.controller.js
@@ -1,78 +1,78 @@
 import { fetchFromTMDB } from "../services/tmdb.service.js";
 
-export async function getTrendingMovie(req, res) {
+export async function getTrendingTv(req, res) {
   try {
     const data = await fetchFromTMDB(
-      "https://api.themoviedb.org/3/trending/movie/day?language=en-US"
+      "https://api.themoviedb.org/3/trending/tv/day?language=en-US"
     );
     const randomMovie =
       data.results[Math.floor(Math.random() * data.results?.length)];
 
     res.json({ success: true, content: randomMovie });
   } catch (error) {
-    console.log("Error on getTrendingMovie function: " + error.message);
+    console.log("Error on getTrendingTv function: " + error.message);
     res.status(500).json({ success: false, message: "Internal server error." });
   }
 }
 
-export async function getMovieTrailers(req, res) {
+export async function getTvTrailers(req, res) {
   try {
     const { id } = req.params;
 
     const data = await fetchFromTMDB(
-      `https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`
+      `https://api.themoviedb.org/3/tv/${id}/videos?language=en-US`
     );
     res.json({ success: true, trailers: data.results });
   } catch (error) {
     if (error.message.includes("404")) {
       return res.status(404).send(null);
     }
-    console.log("Error on getMovieTrailers function: " + error.message);
+    console.log("Error on getTvTrailers function: " + error.message);
     res.status(500).json({ success: false, message: "Internal server error." });
   }
 }
 
-export async function getMovieDetails(req, res) {
+export async function getTvDetails(req, res) {
   const { id } = req.params;
   try {
     const data = await fetchFromTMDB(
-      `https://api.themoviedb.org/3/movie/${id}?language=en-US`
+      `https://api.themoviedb.org/3/tv/${id}?language=en-US`
     );
     res.status(200).json({ success: true, content: data });
   } catch (error) {
     if (error.message.includes("404")) {
       return res.status(404).send(null);
     }
-    console.log("Error on getMovieDetails function: " + error.message);
+    console.log("Error on getTvDetails function: " + error.message);
     res.status(500).json({ success: false, message: "Internal server error." });
   }
 }
 
-export async function getSimilarMovies(req, res) {
+export async function getSimilarTvs(req, res) {
   const { id } = req.params;
   try {
     const data = await fetchFromTMDB(
-      `https://api.themoviedb.org/3/movie/${id}/similar?language=en-US&page=1`
+      `https://api.themoviedb.org/3/tv/${id}/similar?language=en-US&page=1`
     );
     res.status(200).json({ success: true, similar: data.results });
   } catch (error) {
-    console.log("Error on getSimilarMovies function: " + error.message);
+    console.log("Error on getSimilarTvs function: " + error.message);
     res.status(500).json({ success: false, message: "Internal server error." });
   }
 }
 
-export async function getMoviesByCategory(req, res) {
+export async function getTvsByCategory(req, res) {
   const { category } = req.params;
   try {
     const data = await fetchFromTMDB(
-      `https://api.themoviedb.org/3/movie/${category}?language=en-US&page=1`
+      `https://api.themoviedb.org/3/tv/${category}?language=en-US&page=1`
     );
     res.status(200).json({ success: true, content: data.results });
   } catch (error) {
     if (error.message.includes("404")) {
       return res.status(404).send(null);
     }
-    console.log("Error on getMoviesByCategory function: " + error.message);
+    console.log("Error on getTvsByCategory function: " + error.message);
     res.status(500).json({ success: false, message: "Internal server error." });
   }
 }
