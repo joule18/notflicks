420ec77dea4af5f0846d874fdfdd119a05fd7370
joule18 <jrjundam18@gmail.com>
2024-11-21 14:15:43 +0800

    added User model; working signup endpoint

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,10 @@ import { ENV_VARS } from "./config/envVars.js";
 import { connectDB } from "./config/db.js";
 
 const app = express();
+
 const PORT = ENV_VARS.PORT;
+
+app.use(express.json());
 
 app.use("/api/v1/auth", authRoutes);
 
