fe6158b9a94c10dca698dd5dc042d514ebbaa49a
joule18 <jrjundam18@gmail.com>
2024-11-22 17:37:05 +0800

    added watch content page

--- frontend/src/components/MovieSlider.jsx
+++ frontend/src/components/MovieSlider.jsx
@@ -52,22 +52,25 @@ const MovieSlider = ({ category }) => {
         className="flex space-x-4 overflow-x-scroll scrollbar-hide"
         ref={sliderRef}
       >
-        {content.map((item) => (
-          <Link
-            to={`/watch/${item.id}`}
-            className="min-w-[250px] relative group"
-            key={item.id}
-          >
-            <div className="rounded-lg overflow-hidden">
-              <img
-                src={SMALL_IMG_BASE_URL + item.backdrop_path}
-                alt="Movie image"
-                className="transition-transform duration-300 ease-in-out group-hover:scale-125"
-              />
-            </div>
-            <p className="mt-2 text-center">{item.title || item.name}</p>
-          </Link>
-        ))}
+        {content.map((item) => {
+          if (item.backdrop_path === null) return null;
+          return (
+            <Link
+              to={`/watch/${item.id}`}
+              className="min-w-[250px] relative group"
+              key={item.id}
+            >
+              <div className="rounded-lg overflow-hidden">
+                <img
+                  src={SMALL_IMG_BASE_URL + item.backdrop_path}
+                  alt="Movie image"
+                  className="transition-transform duration-300 ease-in-out group-hover:scale-125"
+                />
+              </div>
+              <p className="mt-2 text-center">{item.title || item.name}</p>
+            </Link>
+          );
+        })}
       </div>
 
       {showArrows && (
