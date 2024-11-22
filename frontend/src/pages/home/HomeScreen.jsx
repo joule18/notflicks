81ed6d802243ad00f402f35cabfb1928f2fa27f8
joule18 <jrjundam18@gmail.com>
2024-11-22 16:54:54 +0800

    added dynamic movie sliders in home page

--- frontend/src/pages/home/HomeScreen.jsx
+++ frontend/src/pages/home/HomeScreen.jsx
@@ -7,12 +7,13 @@ import {
   ORIGINAL_IMG_BASE_URL,
   TV_CATEGORIES,
 } from "../../utils/constants";
-
+import { useContentStore } from "../../store/content";
+import MovieSlider from "../../components/MovieSlider";
 import { useState } from "react";
 
 const HomeScreen = () => {
   const { trendingContent } = useGetTrendingContent();
-
+  const { contentType } = useContentStore();
   const [imgLoading, setImgLoading] = useState(true);
 
   if (!trendingContent)
@@ -27,6 +28,10 @@ const HomeScreen = () => {
     <>
       <div className="relative h-screen text-white">
         <Navbar />
+
+        {imgLoading && (
+          <div className="absolute top-0 left-0 w-full h-full bg-black/70 flex items-center justify-center shimmer -z-10" />
+        )}
 
         <img
           src={ORIGINAL_IMG_BASE_URL + trendingContent?.backdrop_path}
@@ -84,6 +89,16 @@ const HomeScreen = () => {
             </Link>
           </div>
         </div>
+      </div>
+
+      <div className="flex flex-col gap-10 bg-black py-10">
+        {contentType === "movie"
+          ? MOVIE_CATEGORIES.map((category) => (
+              <MovieSlider key={category} category={category} />
+            ))
+          : TV_CATEGORIES.map((category) => (
+              <MovieSlider key={category} category={category} />
+            ))}
       </div>
     </>
   );
